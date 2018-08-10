df58009cc8962213976f306f7210c7236debf0b1
Melissa Real <melissa.real@hotmail.com>
2018-08-10 11:05:59 +0000

    automatically backed up by learn

--- lib/author.rb
+++ lib/author.rb
@@ -1,6 +1,7 @@
 class Author
   attr_accessor :name, :author
-  @@counter = 0
+
+
   def initialize(name)
     @name = name
     @posts = []
@@ -9,14 +10,14 @@ class Author
   def add_post(post_title)
     @posts << post_title
     post_title.author  = self
-    @@counter +=1
+
   end
 
   def add_post_by_title(post_title)
     posting = Post.new(post_title)
     @posts << posting
     posting.author = self
-    @@counter +=1
+
   end
 
   def posts
@@ -24,7 +25,7 @@ class Author
   end
 
   def self.post_count
-    @@counter
+    Post.all.count
   end
 
 end
